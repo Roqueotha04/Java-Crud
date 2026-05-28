b00247880b71c08c8f5e94041bc336b8ff75c51d
roqueotha04 <roqueotha04@gmail.com>
2026-05-28 20:49:33 -0300

    add jjwt and method generateToken

---
+++ src/main/java/com/practice/api/utils/JwtUtils.java
@@ -0,0 +1,40 @@
+package com.practice.api.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String privateKey;
+
+    public String generateToken(Authentication authentication){
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(privateKey));
+
+        String username = authentication.getName();
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        String jwtToken= Jwts.builder()
+                .issuer("my-api")
+                .subject(username)
+                .claim("authorities",authorities)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1800000))
+                .signWith(key)
+                .compact();
+
+        return jwtToken;
+    }
+}
